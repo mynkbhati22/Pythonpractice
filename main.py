25563d09c2ec5c11ec79b56b9dda5ee3ff13289b
mynkbhati22 <mayankbhati1911@gmail.com>
2022-11-15 12:55:05 +0530

    created-first-python-project

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-msg = "Hello World"
+msg= "HELLO I AM NEW TO PYTHON"
+print(msg)
+msg= "HELLO I AM NEW TO PYTHON"
+print(msg)
+msg= "HELLO I AM NEW TO PYTHON"
+print(msg)
+msg= "HELLO I AM NEW TO PYTHON"
+print(msg)
+msg= "HELLO I AM NEW TO PYTHON"
 print(msg)
